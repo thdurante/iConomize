e3c05fb8fb15d2158d791ea25b1707f6aa08c5cb
Douglas Soares Pereira <douglasoarespereira@gmail.com>
2016-11-30 16:28:22 -0200

    Added App Icon; Added Tab Icon Added app TintColor;

--- iConomize/AppDelegate.swift
+++ iConomize/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window?.tintColor = UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1)
+        
         return true
     }
 
